fb7ce9e05c8101f492a10c0e441e85accfc8a6c0
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-09-25 14:38:44 +0900

    f

---
+++ SampleCollectionView/SaunaCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  SaunaCollectionViewCell.swift
+//  SampleCollectionView
+//
+//  Created by 渕一真 on 2021/09/25.
+//
+
+import UIKit
+
+final class SaunaCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
--- SampleCollectionView/SaunaViewController.swift
+++ SampleCollectionView/SaunaViewController.swift
@@ -17,7 +17,20 @@ final class SaunaViewController: UIViewController {
     }
     
     private func setupCollectionView() {
+        let nib = UINib(nibName: "SaunaCollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "Cell")
+        
         collectionView.dataSource = self
+        
+        let layout = UICollectionViewFlowLayout()
+        let cellSize = UIScreen.main.bounds.width / 2 - 12
+        
+        layout.itemSize = CGSize(width: cellSize, height: cellSize)
+        layout.sectionInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        layout.minimumInteritemSpacing = 8
+        layout.minimumLineSpacing = 8
+        
+        collectionView.collectionViewLayout = layout
     }
 }
 
