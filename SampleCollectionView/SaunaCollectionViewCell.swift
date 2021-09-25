ddbe54b575b8c21f3303a1c684e428d24e45a6e6
Kazuma Fuchi <fuchi.kazuma@gmail.com>
2021-09-25 16:00:49 +0900

    f

--- SampleCollectionView/SaunaCollectionViewCell.swift
+++ SampleCollectionView/SaunaCollectionViewCell.swift
@@ -12,4 +12,26 @@ final class SaunaCollectionViewCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
     }
+    
+    func setupCollectionViewLayout(collectionView: UICollectionView) {
+        let layout = UICollectionViewFlowLayout()
+        let numberOfColumn: CGFloat = 2
+        let margin: CGFloat = 8
+        let space: CGFloat = margin * numberOfColumn - margin / numberOfColumn
+        
+        let cellSize = UIScreen.main.bounds.width / numberOfColumn - space
+        
+        layout.itemSize = .init(width: cellSize,
+                                height: cellSize)
+        
+        layout.sectionInset = .init(top: margin,
+                                    left: margin,
+                                    bottom: margin,
+                                    right: margin)
+        
+        layout.minimumInteritemSpacing = margin
+        layout.minimumLineSpacing = margin
+        
+        collectionView.collectionViewLayout = layout
+    }
 }
--- SampleCollectionView/SaunaViewController.swift
+++ SampleCollectionView/SaunaViewController.swift
@@ -22,15 +22,8 @@ final class SaunaViewController: UIViewController {
         
         collectionView.dataSource = self
         
-        let layout = UICollectionViewFlowLayout()
-        let cellSize = UIScreen.main.bounds.width / 2 - 12
-        
-        layout.itemSize = CGSize(width: cellSize, height: cellSize)
-        layout.sectionInset = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
-        layout.minimumInteritemSpacing = 8
-        layout.minimumLineSpacing = 8
-        
-        collectionView.collectionViewLayout = layout
+        let layout = SaunaCollectionViewCell()
+        layout.setupCollectionViewLayout(collectionView: collectionView)
     }
 }
 
